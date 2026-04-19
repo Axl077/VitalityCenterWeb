663e21113678fec6d2d3697c0b8271f32ade4027
Axl077 <axeldavidjuanjoalexis@gmail.com>
2026-04-18 22:01:53 -0600

    Corrige orden de inicializacion de app

--- VitalityCenter.js
+++ VitalityCenter.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/health', (req, res) => {
-  res.status(200).send('ok');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/health', (req, res) => {
+  res.status(200).send('ok');
+});
 
 const PORT = process.env.PORT || 3000;
 
